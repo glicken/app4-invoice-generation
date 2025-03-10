936f844158f2a18f3284d76794da6f9421773429
Nicholas <glicken@gmail.com>
2025-03-10 08:15:23 -0700

    Day 26 - Added Invoice Date

--- app4.py
+++ app4.py
@@ -3,17 +3,30 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
+# ensure PDFs folder exists
+output_dir = Path("PDFs")
+output_dir.mkdir(parents=True, exist_ok=True)
+
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     # import filenames:
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+   
+
     # Create PDFs folder abnd enclosed PDFs
     pdf.set_font("Arial", size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
-    pdf.output(f"PDFs/{filename}.pdf")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.set_font("Arial", size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+   
+
+    # Save the PDF file inside the PDFs folder
+    pdf.output(output_dir / f"{filename}.pdf")
     
