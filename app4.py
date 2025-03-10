85fc1359e456aaa69370605cf3900dabfbcaf356
Nicholas <glicken@gmail.com>
2025-03-10 11:02:01 -0700

    Day 26 - Add header row

--- app4.py
+++ app4.py
@@ -10,7 +10,7 @@ output_dir.mkdir(parents=True, exist_ok=True)
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    # Create PDF file
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -19,12 +19,36 @@ for filepath in filepaths:
     invoice_nr, date = filename.split("-")
    
 
-    # Create PDFs folder abnd enclosed PDFs
+    # Create PDFs folder and enclosed PDFs
     pdf.set_font("Arial", size=16, style='B')
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font("Arial", size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=6)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=50, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # add rows
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+    
    
 
     # Save the PDF file inside the PDFs folder
